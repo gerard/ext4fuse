1fe18bae21f1b1d27402b42be3bb3a04fe7359f6
Gerard Lledo <gerard.lledo@gmail.com>
2011-05-02 17:54:56 +0300

    FreeBSD fix for unaligned reads
    Apparently, FreeBSD is not as forgiving as Linux or OSX when it comes to
    unaligned reads of block devices.  This commit introduces one pread(2)
    wrapper that does nothing on Linux but on FreeBSD performs aligned reads.
    That means that for fully unaligned access, there are 3 pread(2) calls
    instead of the usual 1.
    
    Unfortunately, there is some code duplication with op_read.c.  But for
    now let's make this work reasonably on FreeBSD and then let's worry
    about style.  Might be a good idea to get rid of non aligned reads as
    much as possible, and document the rest (by the use of a different API).
    
    Thanks to William Grzybowski for recognizing the problem in the first
    place.

--- disk.c
+++ disk.c
@@ -19,8 +19,70 @@
 #include "disk.h"
 #include "logging.h"
 
+#ifdef __FreeBSD__
+#include <string.h>
+#endif
+
+
 static int disk_fd = -1;
 
+
+static int pread_wrapper(int disk_fd, void *p, size_t size, off_t where)
+{
+#ifdef __FreeBSD__
+#define PREAD_BLOCK_SIZE 1024
+    /* FreeBSD needs to read aligned whole blocks.
+     * TODO: Check what is a safe block size.
+     */
+    static __thread uint8_t block[PREAD_BLOCK_SIZE];
+    off_t first_offset = where % PREAD_BLOCK_SIZE;
+    int ret = 0;
+
+    if (first_offset) {
+        /* This is the case if the read doesn't start on a block boundary.
+         * We still need to read the whole block and we do, but we only copy to
+         * the out pointer the bytes that where actually asked for.  In this
+         * case first_offset is the offset into the block. */
+        int pread_ret = pread(disk_fd, block, PREAD_BLOCK_SIZE, where - first_offset);
+        ASSERT(pread_ret == PREAD_BLOCK_SIZE);
+
+        size_t first_size = MIN(size, (size_t)(PREAD_BLOCK_SIZE - first_offset));
+        memcpy(p, block + first_offset, first_size);
+        p += first_size;
+        size -= first_size;
+        where += first_size;
+        ret += first_size;
+
+        if (!size) return ret;
+    }
+
+    ASSERT(where % PREAD_BLOCK_SIZE == 0);
+
+    size_t mid_read_size = (size / PREAD_BLOCK_SIZE) * PREAD_BLOCK_SIZE;
+    if (mid_read_size) {
+        int pread_ret_mid = pread(disk_fd, p, mid_read_size, where);
+        ASSERT((size_t)pread_ret_mid == mid_read_size);
+
+        p += mid_read_size;
+        size -= mid_read_size;
+        where += mid_read_size;
+        ret += mid_read_size;
+
+        if (!size) return ret;
+    }
+
+    ASSERT(size < PREAD_BLOCK_SIZE);
+
+    int pread_ret_last = pread(disk_fd, block, PREAD_BLOCK_SIZE, where);
+    ASSERT(pread_ret_last == PREAD_BLOCK_SIZE);
+
+    memcpy(p, block, size);
+
+    return ret + size;
+#else
+    return pread(disk_fd, p, size, where);
+#endif
+}
 
 int disk_open(const char *path)
 {
@@ -41,7 +103,7 @@ int __disk_read(off_t where, size_t size, void *p, const char *func, int line)
 
     pthread_mutex_lock(&read_lock);
     DEBUG("Disk Read: 0x%jx +0x%zx [%s:%d]", where, size, func, line);
-    pread_ret = pread(disk_fd, p, size, where);
+    pread_ret = pread_wrapper(disk_fd, p, size, where);
     pthread_mutex_unlock(&read_lock);
     if (size == 0) WARNING("Read operation with 0 size");
 
