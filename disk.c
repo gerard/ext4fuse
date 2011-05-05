7c2c440c9bc01144493a26e51f5402116baa09bc
Brian Tarricone <brian@tarricone.org>
2011-05-05 01:43:26 -0700

    don't try to use FreeBSD pread wrapper on OSX
    OSX also sets __FreeBSD__ in the preprocessor

--- disk.c
+++ disk.c
@@ -29,7 +29,7 @@ static int disk_fd = -1;
 
 static int pread_wrapper(int disk_fd, void *p, size_t size, off_t where)
 {
-#ifdef __FreeBSD__
+#if defined(__FreeBSD__) && !defined(__APPLE__)
 #define PREAD_BLOCK_SIZE 1024
     /* FreeBSD needs to read aligned whole blocks.
      * TODO: Check what is a safe block size.
