f653f7428b7dd355328d2376d9bb33738bc53429
Dan B <git@pwinorth.com>
2011-11-18 07:36:53 -0500

    Fixes for issue #6 and sparse files
    The crash in issue #6 was caused by buf being incremented past the end
    of its allocated space.  This only happened when a file was fragmented
    enough to cause the op_read() loop to run more than twice, so it wasn't
    caught by the existing tests.
    
    This also adds support for sparse files.  A missing pblock is assumed to
    be a "hole" in the file, rather than an error.

---
+++ test/0013-file-integrity-fragmented.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+function t0013 {
+    FUSE_MD5=$(md5sum $MOUNTPOINT/`basename $TMP_FILE` | cut -d\  -f1)
+}
+
+function t0013-check {
+    [ "$FUSE_MD5" = "$FILE_MD5" ]
+}
+
+set -e
+source `dirname $0`/lib.sh
+
+# Make a random file, and store the md5
+TMP_FILE=`mktemp`
+dd if=/dev/urandom of=$TMP_FILE bs=1024 count=1024 &> /dev/null
+FILE_MD5=`md5sum $TMP_FILE | cut -d\  -f1`
+
+e4test_make_LOGFILE
+e4test_make_FS 8
+e4test_make_MOUNTPOINT
+
+# Copy the file in the FS
+e4test_mount
+
+dd if=/dev/urandom of=$MOUNTPOINT/filler bs=1024 count=64 &> /dev/null
+for x in `seq 1 106`; do
+	cp $MOUNTPOINT/filler $MOUNTPOINT/filler.$x
+done
+for x in `seq 2 2 32`; do
+	rm $MOUNTPOINT/filler.$x
+done
+
+cp $TMP_FILE $MOUNTPOINT
+
+e4test_umount
+
+# Check the md5 after mount using fuse
+e4test_fuse_mount
+e4test_run t0013
+e4test_fuse_umount
+
+rm $FS
+rm $TMP_FILE
+
+e4test_end t0013-check
