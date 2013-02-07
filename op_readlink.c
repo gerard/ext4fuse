33f57f632d87dc70f6600d42819ee59f71f15aec
Gerard Lledo <gerard.lledo@gmail.com>
2013-02-07 19:51:46 +0200

    Memory corruption with long symlinks
    So we were happily writing an entire block to the buffer that the readlink
    fuse operation was handling us.  That buffer is not necessarily big enough
    for that.  In the particular case of #22, the buffer is 1025 bytes, while
    the block size is the standard 4K.  Luckily, pread(2) calls us out and
    refuses to corrupt the memory.

--- op_readlink.c
+++ op_readlink.c
@@ -21,7 +21,7 @@
 #include "super.h"
 
 
-static int get_link_dest(struct ext4_inode *inode, char *buf)
+static int get_link_dest(struct ext4_inode *inode, char *buf, size_t bufsize)
 {
     uint64_t inode_size = inode_get_size(inode);
 
@@ -30,7 +30,10 @@ static int get_link_dest(struct ext4_inode *inode, char *buf)
         memcpy(buf, inode->i_block, inode_size);
     } else {
         uint64_t pblock = inode_get_data_pblock(inode, 0, NULL);
-        disk_read_block(pblock, (uint8_t *)buf);
+        char *block_data = malloc(super_block_size());
+        disk_read_block(pblock, (uint8_t *)block_data);
+        strncpy(buf, block_data, bufsize - 1);
+        free(block_data);
     }
 
     buf[inode_size] = 0;
@@ -40,7 +43,6 @@ static int get_link_dest(struct ext4_inode *inode, char *buf)
 /* Check return values, bufer sizes and so on; strings are nasty... */
 int op_readlink(const char *path, char *buf, size_t bufsize)
 {
-    UNUSED(bufsize);            /* FIXME */
     struct ext4_inode inode;
     DEBUG("readlink");
 
@@ -49,7 +51,7 @@ int op_readlink(const char *path, char *buf, size_t bufsize)
         return -EINVAL;
     }
 
-    get_link_dest(&inode, buf);
+    get_link_dest(&inode, buf, bufsize);
     DEBUG("Link resolved: %s => %s", path, buf);
     return 0;
 }
